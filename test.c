391a0ef6bb4a2805cc24537c3690ec616aecd46e
Tifrea Nicoleta <tifreanicoleta24@stud.ase.ro>
2026-03-01 18:32:43 +0200

    afisarea unui mesaj

--- test.c
+++ test.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main() {
-	printf("Salutare!Acesta este repoul cu activitatea mea din cadrul disciplinei Structuri de date!");
+	printf("Salutare!Acesta este repoul cu activitatea mea din cadrul disciplinei Structuri de date! \n");
+
+	//acesta este un comentariu
+
+	printf("Acesta este un fisier de test.");
 	return 0;
 
 }
