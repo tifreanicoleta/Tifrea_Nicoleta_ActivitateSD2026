5ce0be120ce1bd0b5a04cb20afbc6f1723523f04
Tifrea Nicoleta <tifreanicoleta24@stud.ase.ro>
2026-03-01 19:41:44 +0200

    definirea structurii si initializarea acesteia

--- aplicatia01.c
+++ aplicatia01.c
@@ -1,11 +1,38 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 struct Telefon {
-	int;
+	int id;
+	int RAM;
+	char* producator;
+	float pret;
+	char serie;
 };
+
+//initializarea structurii
+
+struct Telefon initializare(int id, int ram, const char* producator, float pret, char serie) { //asemanator constructor cu parametrii din poo
+	struct Telefon t;
+	t.id = id;
+	t.RAM = ram;
+
+	// alocam spatiu pentr char* producator cu malloc
+
+	t.producator = (char*)malloc(sizeof(char) * (strlen(producator) + 1));
+	//copierea in zona de memorie
+
+	strcpy_s(t.producator, strlen(producator)+ 1, producator);
+	t.pret = pret;
+	t.serie = serie;
+	return t;
+}
+
+
 
 
 int main() {
+	struct Telefon t;
+	t = initializare(1, 256, "Samsung", 2000.5, 'A');
+	return 0;
 
 }
