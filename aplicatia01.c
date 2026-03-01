b1fdb41ffef70318b30ee6208ea592c79e7cf488
Tifrea Nicoleta <tifreanicoleta24@stud.ase.ro>
2026-03-01 19:52:16 +0200

    scrierea functiei de afisare si testarea in main

--- aplicatia01.c
+++ aplicatia01.c
@@ -28,11 +28,23 @@ struct Telefon initializare(int id, int ram, const char* producator, float pret,
 }
 
 
+// functia de afisare a structurii telefon
 
+void afisare(struct Telefon t) {
+	printf("%d. Telefonul %s seria %c are %d Gb RAM si costa %5.2f RON \n", t.id, t.producator, t.serie, t.RAM, t.pret);
+}
 
 int main() {
 	struct Telefon t;
 	t = initializare(1, 256, "Samsung", 2000.5, 'A');
+	
+	struct Telefon t1;
+	t1 = initializare(2, 64, "Apple", 3999.99, 'X');
+
+	// testarea functiei de afisare in main
+
+	afisare(t);
+	afisare(t1);
 	return 0;
 
 }
